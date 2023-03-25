20dbc5208188cffbd03c333e1e81369cfb9af48b
Natasha Mundada <44577232+NatashaMundada@users.noreply.github.com>
2023-03-25 12:17:19 -0400

    Delete struct_helper.c

--- struct_helper.c
+++
@@ -1,8 +0,0 @@
-struct memory_segment {
-        void *start;
-        void *end;
-        char permission[4];
-	//char filename[60];
-//        int data_size;
-//        int is_register_context;
-};
